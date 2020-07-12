93022b1da4321c451fd78dfbc6dcb0f1c3b7a5e8
jsiegele <johann.siegele@gmail.com>
2020-07-12 19:56:50 +0200

    Output in JSON Format - erster Wurf

--- x509tojson.sh
+++ x509tojson.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 IFS=''
+
 CERT=$(</dev/stdin)
+
+# TODO: INPUT Validierung:
+#       Input überhaupt vorhanden?
+#       Input valides x509 oder gskit Format?
 
 getCertSubject() {
   echo $CERT | awk 'BEGIN{FS="Subject: "} NF==2{print $2}'
@@ -11,46 +16,76 @@ getCertNotBefore() {
 getCertNotAfter() {
   echo $CERT | awk 'BEGIN{FS="Not After : "} NF==2{print $2}'
 }
-
 getCertSignatureAlgorithm() {
   echo $CERT | awk 'BEGIN{FS="Signature Algorithm: "} NF==2{print $2}'|head -n 1
 }
-
 getCertIssuer() {
   echo $CERT | awk 'BEGIN{FS="Issuer: "} NF==2{print $2}'
 }
-
 getCertNotAfter() {
   echo $CERT | awk 'BEGIN{FS="Not After : "} NF==2{print $2}'
 }
-
 getCertIssuerURL() {
   echo $CERT | awk 'BEGIN{FS="CA Issuers - URI:"} NF==2{print $2}'
 }
 getCertDNS() {
   echo $CERT | sed -n '/Subject Alternative Name:/{n;p;}' | xargs
 }
-
 getCertSerialNumber() {
   echo $CERT | sed -n '/Serial Number:/{n;p;}' | xargs
 }
-
 getCertSubjectKeyIdentifier() {
   echo $CERT | sed -n '/Subject Key Identifier:/{n;p;}' | xargs
 }
-
 getCertAuthorityKeyIdentifier() {
   echo $CERT | sed -n '/Authority Key Identifier:/{n;p;}' | xargs
 }
 
-getCertSubject
-getCertNotBefore
-getCertNotAfter
-getCertSignatureAlgorithm
-getCertIssuer
-getCertNotAfter
-getCertIssuerURL
-getCertDNS
-getCertSerialNumber
-getCertSubjectKeyIdentifier
-getCertAuthorityKeyIdentifier
+SUBJECT=$(getCertSubject)
+ISSUER=$(getCertIssuer)
+
+getCommonName(){
+    echo $1 | awk 'BEGIN{FS="(^| )CN ="} NF==2{print $2}' | awk -F, '{print $1}'| xargs
+}
+getOrganisation(){
+    echo $1 | awk 'BEGIN{FS="(^| )O ="} NF==2{print $2}' | awk -F, '{print $1}'| xargs
+}
+getCountry(){
+    echo $1 | awk 'BEGIN{FS="(^| )C ="} NF==2{print $2}' | awk -F, '{print $1}'| xargs
+}
+
+
+read -r -d '' JSON << EOM
+{
+  "label": "$(getCommonName $SUBJECT)"
+  "date": "$(date)"
+  "subject": {
+    "raw": "$SUBJECT",
+    "common_name": "$(getCommonName $SUBJECT)",
+    "country": "$(getCountry $SUBJECT)",
+    "organization": "$(getOrganisation $SUBJECT)",
+    "names": [
+      "$(getCertDNS)"
+    ]
+  },
+  "issuer": {
+    "raw": "$ISSUER",
+    "common_name": "$(getCommonName $ISSUER)",
+    "country": "$(getCountry $ISSUER)",
+    "organization": "$(getOrganisation $ISSUER)",
+    "url": "$(getCertIssuerURL)"
+  },
+  "serial_number": "$(getCertSerialNumber)",
+  "sans": [
+    "$(getCertDNS)"
+  ],
+  "not_before": "$(getCertNotBefore)",
+  "not_after": "$(getCertNotAfter)",
+  "sigalg": "$(getCertSignatureAlgorithm)",
+  "authority_key_id": "$(getCertAuthorityKeyIdentifier)",
+  "subject_key_id": "$(getCertSubjectKeyIdentifier)"
+}
+EOM
+
+echo "$JSON"
+
